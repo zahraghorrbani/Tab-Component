1a04db342f151ef983ddf5a80c6237fb0dc3b62e
Zahra Ghorbani <zahraghorbani894@gmail.com>
2024-12-29 10:20:42 +0330

    completed tab component

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from 'react';
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const tabData = [
+  {
+    id: 1,
+    title: "ITEM 1",
+    content: "Authorize the user data ✅",
+  },
+  {
+    id: 2,
+    title: "ITEM 2",
+    content: "Redirect user to cart page 🛒",
+  },
+  {
+    id: 3,
+    title: "ITEM 3",
+    content: "Create new payment for the user 💰",
+  },
+];
 
+function App() {
+  const [activeTab, setActiveTab] = useState(1);
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="tab">
+      <div className="tab__header">
+        {
+          tabData.map((item) => {
+            return (
+              <button onClick={() => setActiveTab(item.id)} key={item.id} className={activeTab === item.id ? "active" : ""}>
+                <span>{item.title}</span>
+                <span className="tab-indicator"></span>
+              </button>
+            )
+          })
+        }
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <div className="tab__content">{tabData[activeTab - 1].content}</div>
+    </div>
   )
 }
 
